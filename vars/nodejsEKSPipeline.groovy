f5065658e58442faea3753b5c16bf0162a8a3548
Priyanka <priyanka.rel6@gmail.com>
2025-08-28 13:47:03 +0530

    created nodejsEKS pipeline

--- vars/nodejsEKSPipeline.groovy
+++ vars/nodejsEKSPipeline.groovy
@@ -1,3 +1,96 @@
 def call(Map configMap){
+    pipeline {
+        agent {
+            node {
+            label 'AGENT-1'
+            }
+        }
+        environment {
+            appVersion = ''
+            REGION = 'us-east-1'
+            ACC_ID = '025523569021'
+            PROJECT = configMap.get('project')
+            COMPONENT = Map.get('component')
+        }
+        options {
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds()
+        }
 
+        parameters {
+            booleanParam(name: 'deploy', defaultValue: false, description: 'Toggle this value')
+        }       
+
+        stages{ 
+            stage('Read Package Json') {
+                steps {
+                    script {
+                        def packageJson = readJSON file : 'package.json'
+                        appVersion = packageJson.version
+                        echo "Package version : ${appVersion}"
+                    }
+                }
+            }
+            stage('Installing Dependencies') {
+                steps {
+                    script {
+                        sh """
+                            npm install
+                        """
+                    }
+                }
+            }
+            stage('Unit Testing') {
+                steps {
+                    script {
+                        sh """
+                            echo "Unit Testing"
+                        """
+                    }
+                }
+            }
+            stage('Docker Build') {
+                steps {
+                    script {
+                        withAWS(credentials: 'aws-creds', region: '${REGION}') {
+                            sh """
+                                aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com
+                                docker build -t ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion} .
+                                docker push ${ACC_ID}.dkr.ecr.us-east-1.amazonaws.com/${PROJECT}/${COMPONENT}:${appVersion}
+                            """
+                        }
+                    }
+                }
+            }
+            stage('Trigger Deploy') {
+                when{
+                    expression { params.deploy }
+                }
+                steps {
+                    script {
+                        build job: 'catalogue-cd',
+                        parameters: [
+                            string(name: 'appVersion', value: "${appVersion}"),
+                            string(name: 'deploy_to', value: 'dev')
+                        ],
+                        propagate: false,  // even SG fails VPC will not be effected
+                        wait: false // VPC will not wait for SG pipeline completion
+                    }
+                }
+            }
+        }
+
+        post {
+            always {
+                echo 'I will always says Hello again!'
+                deleteDir()
+            }
+            success {
+                echo 'Hello Success'
+            }
+            failure {
+                echo 'Hello Failure'
+            }
+        }
+    }
 }
